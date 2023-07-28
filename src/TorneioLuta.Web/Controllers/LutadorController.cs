2006286570b6d7b42a1e03b7848ea3f3cc46be0f
Matheus Souza <matheus-pereira2011@hotmail.com>
2023-07-28 20:41:41 -0300

    Inicial

---
+++ src/TorneioLuta.Web/Controllers/LutadorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TorneioLuta.Dominio.Interfaces.Servicos;
+
+namespace TorneioLuta.Web.Controllers
+{
+    public class LutadorController : Controller
+    {
+        private readonly ILogger<LutadorController> _logger;
+        private readonly ILutadoresServico _servico;
+
+        public LutadorController(ILogger<LutadorController> logger,
+            ILutadoresServico servico)
+        {
+            _logger = logger;
+            _servico = servico;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var requestLutadores = await _servico.BuscaTodosLutadoresAsync();
+            //Pegar retorno dos lutadores e converter em uma viewModel
+
+            return View(requestLutadores);
+        }
+    }
+}
